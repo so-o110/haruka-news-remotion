006ed0a09668a6c0b5de9f0c2bd885b212659782
so <nakaso@yahoo.ne.jp>
2026-06-11 21:18:55 +0900

    Initial Haruka News Remotion project

---
+++ src/Composition.tsx
@@ -0,0 +1,19 @@
+export const MyComposition = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#111827",
+        color: "white",
+        fontSize: 80,
+        fontWeight: "bold",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      ハルカニュース
+    </div>
+  );
+};
